ce6012b24e6ff90fb98e0a2b61c6218f469e21bc
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-09-07 19:32:22 +0600

    few things added 2

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -8,7 +8,7 @@ const userController_1 = require("../controllers/userController");
 const authenticate_1 = require("../middlewares/authenticate");
 const authorize_1 = require("../middlewares/authorize");
 const router = express_1.default.Router();
-router.get("/me", authenticate_1.authenticate, (0, authorize_1.authorize)("admin"), userController_1.getAllUser);
+router.get("/", authenticate_1.authenticate, (0, authorize_1.authorize)("admin"), userController_1.getAllUser);
 // Get user profile
 router.get("/me", authenticate_1.authenticate, userController_1.getProfile);
 // Update user profile
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import { getAllUser, getProfile, updateProfile } from "../controllers/userController";
+import {
+  getAllUser,
+  getProfile,
+  updateProfile,
+} from "../controllers/userController";
 import { authenticate } from "../middlewares/authenticate";
 import { authorize } from "../middlewares/authorize";
 
 const router = express.Router();
 
-
-router.get("/me", authenticate,authorize("admin"), getAllUser);
+router.get("/", authenticate, authorize("admin"), getAllUser);
 // Get user profile
 router.get("/me", authenticate, getProfile);
 
